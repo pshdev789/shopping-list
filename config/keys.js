dfd1841d4083c9865dec02651ba4526477271cc9
pshdev789 <68139465+pshdev789@users.noreply.github.com>
2020-09-22 08:08:59 +0530

    Added config key file and server.js

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+mongodb+srv://kanchi:v@2hpsk#@mongodb01.gs0ya.mongodb.net/<dbname>?retryWrites=true&w=majority
+
+module.exports = {
+    mongoURI:'mongodb+srv://kanchi:kanchi@mongodb01-gs0ya.mongodb.net/mern-shopping?retryWrites=true&w=majority'
+}
