47ed66d70a00a198d9e3dbce42999147571c2801
pshdev789 <68139465+pshdev789@users.noreply.github.com>
2020-09-23 14:33:10 +0530

    Refactor code
    Added misconfigured modifications to keys and package.json

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,3 @@
-mongodb+srv://kanchi:v@2hpsk#@mongodb01.gs0ya.mongodb.net/<dbname>?retryWrites=true&w=majority
-
 module.exports = {
-    mongoURI:'mongodb+srv://kanchi:kanchi@mongodb01-gs0ya.mongodb.net/mern-shopping?retryWrites=true&w=majority'
+    mongoURI:'mongodb+srv://kanchi:kanchi@mongodb01-gs0ya.mongodb.net/mern-shoppin?retryWrites=true&w=majority'
 }
