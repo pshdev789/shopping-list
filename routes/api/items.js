5400d1d0db6488d454d28e016ac5b8042e9ad957
pshdev789 <68139465+pshdev789@users.noreply.github.com>
2020-09-25 21:52:59 +0530

    Update items.js
    Added post method to create an item

--- routes/api/items.js
+++ routes/api/items.js
@@ -7,11 +7,22 @@ const Item = require('../../models/Item');
 // @route GET api/items
 // @desc Get All Items
 // @access Public 
-
+// sort in descending order
 router.get('/', (req, res) => {
  Item.find()
     .sort({date: -1 })
     .then(items => res.json(items))
 });
 
+
+// @route POST api/items
+// @desc Create an Item
+// @access Public 
+// sort in descending order
+router.post('/', (req, res) => {
+    const newItem =  new Item({
+       name: req.body.name
+    });
+    newItem.save().then(item => res.json(item));
+   });
 module.exports = router;
